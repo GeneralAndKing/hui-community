1915f3b43535616425bf07ca81982a442f2a17a9
lizhongyue <lizhongyue248@163.com>
2024-10-14 11:33:37 +0800

    🎉 初始化项目

---
+++ hui-service/src/main/java/wiki/zyue/hui/service/HuiServiceApplication.java
@@ -0,0 +1,13 @@
+package wiki.zyue.hui.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuiServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HuiServiceApplication.class, args);
+  }
+
+}
---
+++ hui-service/src/test/java/wiki/zyue/hui/service/HuiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wiki.zyue.hui.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuiServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
