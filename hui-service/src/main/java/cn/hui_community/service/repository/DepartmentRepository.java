b7941427db3a60302803f5444425abbf52727cbd
klein <fanguorui@metatrust.io>
2024-10-24 11:58:08 +0800

    feat: remove department

--- hui-service/src/main/java/cn/hui_community/service/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.hui_community.service.repository;
-
-import cn.hui_community.service.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, String> {
-}
