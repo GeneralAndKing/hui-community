42387c9a001d760d1217e876c1f0e4119066ed70
lizhongyue <lizhongyue248@163.com>
2024-11-25 12:56:29 +0800

    feat: add api lib

--- hui-admin/vite.config.ts
+++ hui-admin/vite.config.ts
@@ -3,12 +3,26 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import Components from "unplugin-vue-components/vite";
+import VueRouter from "unplugin-vue-router/vite";
 import vueDevTools from "vite-plugin-vue-devtools";
 import UnoCSS from "unocss/vite";
+import { TDesignResolver } from "unplugin-vue-components/resolvers";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), UnoCSS()],
+  plugins: [
+    VueRouter({
+      /* options */
+    }),
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    UnoCSS(),
+    Components({
+      resolvers: [TDesignResolver()]
+    })
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
