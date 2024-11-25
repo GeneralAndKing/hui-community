31a97be7b57b3c4cc4266ea0abf59600b0d6380e
lizhongyue <lizhongyue248@163.com>
2024-11-25 11:55:21 +0800

    feat: add biome lint

--- hui-admin/src/main.ts
+++ hui-admin/src/main.ts
@@ -1,18 +1,18 @@
-import './assets/main.css'
-import 'virtual:uno.css'
+import "./assets/main.css";
+import "virtual:uno.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
-import TDesign from 'tdesign-vue-next'
-import 'tdesign-vue-next/es/style/index.css'
-import App from './App.vue'
-import router from './router'
+import TDesign from "tdesign-vue-next";
+import "tdesign-vue-next/es/style/index.css";
+import App from "./App.vue";
+import router from "./router";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.use(TDesign)
+app.use(createPinia());
+app.use(router);
+app.use(TDesign);
 
-app.mount('#app')
+app.mount("#app");
