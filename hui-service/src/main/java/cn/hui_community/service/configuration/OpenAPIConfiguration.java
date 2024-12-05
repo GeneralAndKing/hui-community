eedba2344946f02a9bb8a6341d4f525c596f4208
klein <fanguorui@metatrust.io>
2024-12-05 23:47:01 +0800

    fix: fixed swagger

--- hui-service/src/main/java/cn/hui_community/service/configuration/OpenAPIConfiguration.java
+++ hui-service/src/main/java/cn/hui_community/service/configuration/OpenAPIConfiguration.java
@@ -3,10 +3,9 @@ package cn.hui_community.service.configuration;
 import cn.hui_community.service.configuration.security.authentication.password.SysUserPasswordAuthenticationFilter;
 import cn.hui_community.service.configuration.security.authentication.token.RefreshTokenAuthenticationFilter;
 import cn.hui_community.service.configuration.security.authentication.wechat.UserWechatMiniAuthenticationFilter;
-import cn.hui_community.service.model.Token;
+import cn.hui_community.service.model.dto.response.TokenResponse;
 import io.swagger.v3.core.converter.ModelConverters;
 import io.swagger.v3.core.converter.ResolvedSchema;
-import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
@@ -17,20 +16,15 @@ import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
-import io.swagger.v3.oas.models.media.Schema;
-import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.parameters.RequestBody;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.Triple;
 import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @OpenAPIDefinition(
         info = @Info(title = "Hui-Community Doc", version = "0.0.1"),
@@ -59,7 +53,7 @@ public class OpenAPIConfiguration {
 
     @Bean
     public OpenApiCustomizer customOpenApiCustomizer() {
-        ResolvedSchema responseSchema = ModelConverters.getInstance().readAllAsResolvedSchema(Token.class);
+        ResolvedSchema responseSchema = ModelConverters.getInstance().readAllAsResolvedSchema(TokenResponse.class);
         return openApi -> {
             List.of(Pair.of("/sys-api/login", SysUserPasswordAuthenticationFilter.Request.class),
                     Pair.of("/user-api/login", UserWechatMiniAuthenticationFilter.Request.class),
