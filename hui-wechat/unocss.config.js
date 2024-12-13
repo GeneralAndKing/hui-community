d44bc91130a7b542f0ab3648f107e4da08ae5456
lizhongyue <lizhongyue248@163.com>
2024-12-13 14:29:46 +0800

    feat: add mine page

--- hui-wechat/unocss.config.js
+++ hui-wechat/unocss.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from "unocss";
+import {defineConfig} from "unocss";
 import presetWeapp from 'unocss-preset-weapp'
 
 const include = [/\.wxml$/]
 
 export default defineConfig({
-  content:{
-    pipeline:{
+  content: {
+    pipeline: {
       include
     }
   },
-  presets: [
-    presetWeapp(),
-  ],
-  separators:'__'
+  presets: [presetWeapp(),],
+  rules: [['card-data', {"box-shadow": "var(--td-tab-bar-round-shadow, var(--td-shadow-3, 0 6px 30px 5px rgba(0, 0, 0, 0.05), 0 16px 24px 2px rgba(0, 0, 0, 0.04), 0 8px 10px -5px rgba(0, 0, 0, 0.08)))"}]],
+  separators: '__'
 })
